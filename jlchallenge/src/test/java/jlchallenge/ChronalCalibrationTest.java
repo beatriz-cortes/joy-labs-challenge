e0a4d982ad73b94fae6498450a0a1999c8f1ea5b
Work <Work@Home>
2019-06-24 19:16:07 -0500

    Cleaning code

--- jlchallenge/src/test/java/jlchallenge/ChronalCalibrationTest.java
+++ jlchallenge/src/test/java/jlchallenge/ChronalCalibrationTest.java
@@ -2,9 +2,6 @@ package jlchallenge;
 
 import static org.junit.Assert.*;
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ChronalCalibrationTest {
